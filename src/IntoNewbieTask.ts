50bdffd51ac3890779b2f969dc9b21ba3ceb2f2e
calmw <calm.wang@hotmail.com>
2023-10-09 22:24:33 +0800

    Initial commit

--- src/IntoNewbieTask.ts
+++ src/IntoNewbieTask.ts
@@ -4,21 +4,15 @@ import {NewbieTask} from "../generated/schema"
 
 export function handleClaimRecordLog(event: ClaimRecordEvent): void {
 
-  let t_id = "OTC-".concat(event.params.tid.toString())
-    // let packageObj = NewbieTask.load(o_id)
-    // if (packageObj == null) {
-      let  packageObj = new NewbieTask(t_id)
-        packageObj.user = event.params.user
-        packageObj.claimType = event.params.claimType
-        packageObj.amount = event.params.amount
-        packageObj.ctime = event.params.ctime
-        packageObj.save()
-    // } else {
-    //     packageObj.user = event.params.user
-    //     packageObj.amount = event.params.amount
-    //     packageObj.claimType = event.params.claimType
-    //     packageObj.ctime = event.params.ctime
-    //     packageObj.save()
-    // }
+    let t_id = event.params.tid.toString()
+    let packageObj = NewbieTask.load(t_id)
+    if (packageObj == null) {
+        packageObj = new NewbieTask(t_id)
+    }
+    packageObj.user = event.params.user
+    packageObj.claimType = event.params.claimType
+    packageObj.amount = event.params.amount
+    packageObj.ctime = event.params.ctime
+    packageObj.save()
 }
 
