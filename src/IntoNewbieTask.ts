c294f3279a5693f5b51b53a354b52ab72d6d1be0
calmw <calm.wang@hotmail.com>
2023-10-10 00:36:53 +0800

    Initial commit

--- src/IntoNewbieTask.ts
+++ src/IntoNewbieTask.ts
@@ -4,7 +4,8 @@ import {Address, BigInt, Bytes} from "@graphprotocol/graph-ts";
 
 
 export function handleClaimRecordLog(event: ClaimRecordEvent): void {
-    let newbieTask = new NewbieTask(createEventID(event.block.number, event.logIndex))
+    // let newbieTask = new NewbieTask(createEventID(event.block.number, event.logIndex))
+    let newbieTask = new NewbieTask(event.params.tid.toString())
     newbieTask.tid = event.params.tid
     newbieTask.user = event.params.user
     newbieTask.claimType = event.params.claimType
